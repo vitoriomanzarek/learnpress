9a5abe430e716194eada0c7b4710b9a43511c71c
DoNgocPhuc <phucngocdo@gmail.com>
2023-04-24 14:52:47 +0700

    search

--- templates/admin/order-list/search.php
+++ templates/admin/order-list/search.php
@@ -13,13 +13,11 @@ if ( ! isset( $data ) ) {
 	<input
 		type="search" id="order-id" name="order-id" value=""
 		placeholder="<?php esc_attr_e( 'Order ID', 'learnpress' ); ?>"
-		style="width: 300px;"
 	>
 
 	<input
 		type="search" id="course-name" name="course-name" value=""
 		placeholder="<?php esc_attr_e( 'Course Name', 'learnpress' ); ?>"
-		style="width: 300px;"
 	>
 	<input type="submit" id="search-submit" class="button"
 		   value="<?php esc_html_e( 'Search Orders', 'learnpress' ); ?>">
