e7e631081b5af8186da02a55980a3c66d9e94759
Tungnx <tungnxt89@gmail.com>
2023-09-27 11:37:04 +0700

    = 4.2.4 = ~ Map stdclass to Model

--- inc/Models/Course/CourseSectionItemModel.php
+++ inc/Models/Course/CourseSectionItemModel.php
@@ -8,6 +8,8 @@
 
 namespace LearnPress\Models\Course;
 
+use stdClass;
+
 class CourseSectionItemModel {
 	/**
 	 * @var int primary key,
@@ -29,4 +31,35 @@ class CourseSectionItemModel {
 	 * @var string type of item.
 	 */
 	public $item_type;
+
+	/**
+	 * Mapper stdclass to model
+	 *
+	 * @param stdClass $object
+	 * @return CourseSectionItemModel
+	 */
+	public function map_stdclass( stdClass $object ): self {
+		$course_section_item = new self();
+
+		foreach ( $object as $key => $value ) {
+			if ( property_exists( $course_section_item, $key ) ) {
+				$course_section_item->{$key} = $value;
+			}
+
+			//Todo: For old data of LP version lower 4.2.4, when run along time, will remove this code
+			switch ( $key ) {
+				case 'id':
+					$course_section_item->item_id = $value;
+					break;
+				case 'order':
+					$course_section_item->item_order = $value;
+					break;
+				case 'type':
+					$course_section_item->item_type = $value;
+					break;
+			}
+		}
+
+		return $course_section_item;
+	}
 }
--- inc/Models/Course/CourseSectionModel.php
+++ inc/Models/Course/CourseSectionModel.php
@@ -8,6 +8,8 @@
 
 namespace LearnPress\Models\Course;
 
+use stdClass;
+
 class CourseSectionModel {
 	/**
 	 * @var int primary key
@@ -33,4 +35,47 @@ class CourseSectionModel {
 	 * @var CourseSectionItemModel[]
 	 */
 	public $items;
+
+	/**
+	 * Mapper stdclass to model
+	 *
+	 * @param stdClass $object
+	 * @return CourseSectionModel
+	 */
+	public function map_stdclass( stdClass $object ): self {
+		$course_section = new self();
+
+		foreach ( $object as $key => $value ) {
+			if ( property_exists( $course_section, $key ) ) {
+				$course_section->{$key} = $value;
+
+				if ( 'items' === $key ) {
+					/**
+					 * @var stdClass $item
+					 */
+					foreach ( $course_section->items as $k_item => $item ) {
+						$course_section->items->{$k_item} = ( new CourseSectionItemModel() )->map_stdclass( $item );
+					}
+				}
+
+				//Todo: For old data of LP version lower 4.2.4, when run along time, will remove this code
+				switch ( $key ) {
+					case 'id':
+						$course_section->section_id = $value;
+						break;
+					case 'order':
+						$course_section->section_order = $value;
+						break;
+					case 'title':
+						$course_section->section_name = $value;
+						break;
+					case 'description':
+						$course_section->section_description = $value;
+						break;
+				}
+			}
+		}
+
+		return $course_section;
+	}
 }
